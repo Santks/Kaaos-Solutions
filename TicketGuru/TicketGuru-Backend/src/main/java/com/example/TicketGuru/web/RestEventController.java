fb1f369d76268f630516d3bedb0a65befdf537a1
Sauria1 <bhd514@myy.haaga-helia.fi>
2024-02-25 22:36:31 +0200

    POST lisätty

--- TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/web/RestEventController.java
+++ TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/web/RestEventController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +25,11 @@ public class RestEventController {
 	//GET
 	
 	//POST
+	@PostMapping("/events")
+	Event createEvent(@RequestBody Event newEvent) {
+		log.info("Create new event");
+		return eventRepo.save(newEvent);
+	}
 	
 	//PUT
 	@PutMapping("/events/{id}")
