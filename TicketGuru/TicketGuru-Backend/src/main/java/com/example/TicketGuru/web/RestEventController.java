9941eb5accbddf1df7b96583e0b31cdc414b17a8
dskmbbltd <14019485+dskmbbltd@users.noreply.github.com>
2024-02-25 21:15:23 +0200

    tapahtuman rest kontrolleri barebones

---
+++ TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/web/RestEventController.java
@@ -0,0 +1,22 @@
+package com.example.TicketGuru.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.example.TicketGuru.domain.EventRepository;
+
+@Controller
+public class RestEventController {
+	@Autowired
+	private EventRepository eventRepo;
+	
+//	Tänne event rest-rajapinnan metodit
+	
+	//GET
+	
+	//POST
+	
+	//PUT
+	
+	//DELETE
+}
