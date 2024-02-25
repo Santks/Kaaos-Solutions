2b59c03af1ed8688ba44f7d2da09b7429c0966d4
Santks <santeri.kuronen@myy.haaga-helia.fi>
2024-02-25 21:55:54 +0200

    put metodi lisätty

--- TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/web/RestEventController.java
+++ TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/web/RestEventController.java
@@ -1,12 +1,21 @@
 package com.example.TicketGuru.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.TicketGuru.domain.Event;
 import com.example.TicketGuru.domain.EventRepository;
 
-@Controller
+@RestController
 public class RestEventController {
+	
+	private static final Logger log = LoggerFactory.getLogger(RestEventController.class);
+	
 	@Autowired
 	private EventRepository eventRepo;
 	
@@ -17,6 +26,12 @@ public class RestEventController {
 	//POST
 	
 	//PUT
+	@PutMapping("/events/{id}")
+	Event editEventRest(@RequestBody Event editedEvent, @PathVariable Long id) {
+		log.info("Edit event by id");
+		editedEvent.setId(id);
+		return eventRepo.save(editedEvent);
+	}
 	
 	//DELETE
 }
