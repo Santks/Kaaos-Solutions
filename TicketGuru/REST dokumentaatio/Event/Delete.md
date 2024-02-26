659d838e6bd102cfb800b5b828ce318fa476f4f7
dskmbbltd <14019485+dskmbbltd@users.noreply.github.com>
2024-02-26 15:46:13 +0200

    muokattu delete + delete dokumentaatio

---
+++ TicketGuru/REST dokumentaatio/Event/Delete.md
@@ -0,0 +1,20 @@
+## Päivitä tapahtuma
+
+Tapahtuman järjestäjä voi poistaa yksittäisen tapahtuman
+
+**URL**: `/api/events/{id}`
+
+**Metodi**:`DELETE`
+
+**Autentikointi vaaditaan**: 
+
+**Tarvittavat oikeudet**: 
+
+**Data-rajoitukset**
+
+**Response**
+Code:`200 OK`
+Tapahtuma poistettu onnistuneesti.
+
+Code:`404 Not Found`
+Tapahtumaa ei löydetty.
--- TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/web/RestEventController.java
+++ TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/web/RestEventController.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -65,8 +66,12 @@ public class RestEventController {
 	
 	//DELETE
 	@DeleteMapping("/events/{id}")
-	void deleteEvent(@PathVariable Long id) {
+	ResponseEntity<String> deleteEvent(@PathVariable Long id) {
 		log.info("Delete event by id");
+		if (eventRepo.findById(id).isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
 		eventRepo.deleteById(id);
+		return ResponseEntity.ok("Poistettu");
 	}
 }
