f439558b701bae80b133796c097436aed79b175b
Sauria1 <bhd514@myy.haaga-helia.fi>
2024-03-14 15:28:20 +0200

    Autentikointi luotu

---
+++ TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.TicketGuru;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+        .inMemoryAuthentication()
+        .withUser("user")
+        .password(passwordEncoder.encode("user"))
+        .authorities("ROLE_USER");
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(expressionInterceptUrlRegistry ->
+        expressionInterceptUrlRegistry
+        .requestMatchers("/securityNone").permitAll()
+        .anyRequest().authenticated())
+        .httpBasic(HttpSecurityHttpBasicConfigurer -> HttpSecurityHttpBasicConfigurer.authenticationEntryPoint(new
+        HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
+        return http.build();
+    }
+}
