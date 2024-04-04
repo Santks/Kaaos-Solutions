49fbc6a9fb9d4e3da4663b60b0f03b1ffb930286
Sauria1 <bhd514@myy.haaga-helia.fi>
2024-04-04 15:37:25 +0300

    Alustava RestVenueController

---
+++ TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/web/RestVenueController.java
@@ -0,0 +1,26 @@
+package com.example.TicketGuru.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.TicketGuru.domain.VenueRepository;
+
+@RestController
+public class RestVenueController {
+
+    private static final Logger log = LoggerFactory.getLogger(RestEventController.class);
+
+    @Autowired
+    private VenueRepository venueRepo;
+
+    //GET
+
+    // POST
+
+    // PUT
+
+    // DELETE
+   
+}
