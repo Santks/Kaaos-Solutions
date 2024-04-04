a5942289dd25c35f11b60f0df3fc80f587ae1beb
Sauria1 <bhd514@myy.haaga-helia.fi>
2024-04-04 16:23:47 +0300

    RestVenue POST function added

--- TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/web/RestVenueController.java
+++ TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/web/RestVenueController.java
@@ -3,8 +3,14 @@ package com.example.TicketGuru.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.TicketGuru.domain.Venue;
 import com.example.TicketGuru.domain.VenueRepository;
 
 @RestController
@@ -15,12 +21,25 @@ public class RestVenueController {
     @Autowired
     private VenueRepository venueRepo;
 
-    //GET
+    // GET
 
     // POST
+    @PostMapping("/venues")
+    public ResponseEntity<Venue> createVenue(@RequestBody Venue newVenue) {
+        log.info("Create new venue");
+        if (newVenue.getName() == null || newVenue.getAddress() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST); // 400: Bad Request!
+        }
+        try {
+            return new ResponseEntity<>(venueRepo.save(newVenue), HttpStatus.CREATED); // 201: Created!
+        } catch (Exception e) {
+            log.error("Error creating venue", e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR); // 500: Internal Server Error!
+        }
+    }
 
     // PUT
 
     // DELETE
-   
+
 }
