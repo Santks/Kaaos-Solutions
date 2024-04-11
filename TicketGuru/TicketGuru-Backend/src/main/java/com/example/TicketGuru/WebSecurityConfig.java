73c3105afa7a0bc66bb019903179507e36d97808
Sauria1 <bhd514@myy.haaga-helia.fi>
2024-04-11 19:55:28 +0300

    Poistettu tarpeeton import

--- TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/WebSecurityConfig.java
+++ TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/WebSecurityConfig.java
@@ -1,8 +1,6 @@
 package com.example.TicketGuru;
 
 import java.util.Arrays;
-import java.util.Collections;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
