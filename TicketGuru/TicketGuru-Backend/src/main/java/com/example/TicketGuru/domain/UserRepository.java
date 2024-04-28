0face9604257ab4b8ce61f047176bed9a9fbf985
dskmbbltd <14019485+dskmbbltd@users.noreply.github.com>
2024-04-28 17:55:29 +0300

    userrepo

--- TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/domain/UserRepository.java
+++ TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/domain/UserRepository.java
@@ -1,9 +1,12 @@
 package com.example.TicketGuru.domain;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-
+	
+	Optional<User> findUserByEmail(String email);
 }
