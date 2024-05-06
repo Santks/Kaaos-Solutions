a98dcff4769fc07bcd01d1c03592df24b6b1b69f
Sauria1 <bhd514@myy.haaga-helia.fi>
2024-05-06 21:35:39 +0300

    EventReport toinen päivitys

--- TicketGuru/TicketGuru-Client/src/components/EventReport.jsx
+++ TicketGuru/TicketGuru-Client/src/components/EventReport.jsx
@@ -6,31 +6,44 @@ import { fetchEventTickets } from './EventHandler';
 import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
 import { Button } from '@mui/material';
 
-const EventReport = ({ eventId }) => {
+const EventReport = () => {
 
     const [ticketInfo, setTicketInfo] = useState([]);
+    
+    const eventId = window.location.pathname.split('/').pop();
     
     useEffect(() => {
         fetchEventTickets(eventId)
             .then(data => setTicketInfo(data))
-            .catch(error => console.error('Error:', error));
+            .catch(error => console.error('Error:', error));    
     }, [eventId]);
 
     const columnDefs = [
+        { headerName: 'Ticket Type', field: 'null'},
+        { headerName: 'Total Tickets', field: 'null'},
         { headerName: 'Ticket ID', field: 'ticketId'},
         { headerName: 'Price', field: 'price'},
     ];
 
+    const defaultColDef = {
+        flex: 1
+    };
+
     return (
-        <div className="ag-theme-material" style={{ height: 400, width: '100%' }}>
+        <>
+        <h1>Sales Report</h1>
+        <div className="ag-theme-material" style={{ height: 500, width: '100%'}}>
             <AgGridReact
                 rowData={ticketInfo}
                 columnDefs={columnDefs}
+                defaultColDef={defaultColDef}
                 pagination={true}
-                paginationPageSize={5}
+                paginationAutoPageSize={true}
             />
-            <Button style={{ marginBottom: "10px", marginLeft: "10px" }} color="primary" variant="contained">Order List <ReceiptLongIcon/></Button>
         </div>
+        <h3>Total Tickets: {ticketInfo ? ticketInfo.length : 0}</h3>
+        <Button style={{ marginLeft: "10px", marginTop: "40px" }} color="primary" variant="contained">Order List <ReceiptLongIcon/></Button>
+        </>
     );
 };
 
