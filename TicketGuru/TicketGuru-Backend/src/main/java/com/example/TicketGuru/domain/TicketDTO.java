540bbee0c0486de9acc1dde7e5dcd0f9ab8c0007
dskmbbltd <14019485+dskmbbltd@users.noreply.github.com>
2024-05-09 20:09:20 +0300

    ticketDTO shows ticketprice

--- TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/domain/TicketDTO.java
+++ TicketGuru/TicketGuru-Backend/src/main/java/com/example/TicketGuru/domain/TicketDTO.java
@@ -1,66 +1,54 @@
 package com.example.TicketGuru.domain;
+
 import java.util.UUID;
 import java.time.LocalDateTime;
+
 public class TicketDTO {
 
 	private Long ticketId;
-    private Long eventId;
-    private Long ticketTypeId;
-    private UUID uuid;
-    private LocalDateTime ticketUsed;
+	private Long eventId;
+	private Long ticketTypeId;
+	private Double ticketPrice;
+	private UUID uuid;
+	private LocalDateTime ticketUsed;
 
+	public TicketDTO(Ticket ticket) {
+		this.ticketId = ticket.getTicketId();
+		this.ticketTypeId = ticket.getTicketType().getTicketTypeId();
+		this.ticketPrice = ticket.getTicketType().getPrice();
+		this.eventId = ticket.getEvent().getId();
+		this.uuid = ticket.getUuid();
+		this.ticketUsed = ticket.getTicketUsed();
 
-
-    public TicketDTO(Ticket ticket) {
-    	this.ticketId = ticket.getTicketId();
-        this.ticketTypeId = ticket.getTicketType().getTicketTypeId();
-        this.eventId = ticket.getEvent().getId();
-        this.uuid = ticket.getUuid();
-        this.ticketUsed = ticket.getTicketUsed();
-   
-    }
-
-    public Long getTicketId() {
-		return ticketId;
 	}
 
-
+	public Long getTicketId() {
+		return ticketId;
+	}
 
 	public void setTicketId(Long ticketId) {
 		this.ticketId = ticketId;
 	}
 
-
-
 	public Long getEventId() {
 		return eventId;
 	}
-
-
 
 	public void setEventId(Long eventId) {
 		this.eventId = eventId;
 	}
 
-
-
 	public Long getTicketTypeId() {
 		return ticketTypeId;
 	}
-
-
 
 	public void setTicketTypeId(Long ticketTypeId) {
 		this.ticketTypeId = ticketTypeId;
 	}
 
-
-
 	public UUID getUuid() {
 		return uuid;
 	}
-
-
 
 	public void setUuid(UUID uuid) {
 		this.uuid = uuid;
@@ -72,5 +60,13 @@ public class TicketDTO {
 
 	public void setTicketUsed(LocalDateTime ticketUsed) {
 		this.ticketUsed = ticketUsed;
+	}
+
+	public Double getTicketPrice() {
+		return ticketPrice;
+	}
+
+	public void setTicketPrice(Double ticketPrice) {
+		this.ticketPrice = ticketPrice;
 	}
 }
